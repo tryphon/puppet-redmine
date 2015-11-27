e1dd684d78a9e847ef6f9b012b5590a3456f2288
Alban Peignier <alban@tryphon.eu>
2015-11-27 22:19:02 +0100

    Fixes api_key in redmine::git_hook

--- manifests/git_hook.pp
+++ manifests/git_hook.pp
@@ -1,6 +1,6 @@
 define redmine::git_hook($hostname, $api_key) {
   file { "/srv/git/$name/hooks/post-receive.d/redmine":
-    content => "#!/bin/sh\nwget --quiet -O /dev/null 'http://${hostname}/sys/fetch_changesets?key=${api}'\n",
+    content => "#!/bin/sh\nwget --quiet -O /dev/null 'http://${hostname}/sys/fetch_changesets?key=${api_key}'\n",
     mode => 755,
     require => Git::Repository[$name]
   }
